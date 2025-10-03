dcefc2094a6718fc735610f71f93d2ec306bc4d5
Steven Harvie <1035350@student.sae.edu.au>
2025-10-03 16:33:03 +1000

    making the hammer a prefab, instantiating the object and assigning as a child to the player object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,51 @@
+using System;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace SAE.GAD176.ProjectOne.Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Here should the player script that will work with
+    /// prefab object being attached to the player when item is picked up
+    /// player health
+    /// player movement
+    /// </summary>
+    public class Player : MonoBehaviour
     {
-        
-    }
+        // [SerializeField] private float movementSpeed = 5f; 
+        [SerializeField] private GameObject usableItem;
+        private Transform parentObject;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            ItemTransformPosition();
+        }
+        void Update()
+        {
+            
+        }
+
+        public void ItemTransformPosition()
+        {
+            // instantiate the object as the prefab item
+            GameObject instantiatedObject = Instantiate(usableItem);
+            
+            // make the instantiated item a child of the player object
+            instantiatedObject.transform.SetParent(this.transform);
+            
+            // set the local position of the prefab offset by a bit
+            instantiatedObject.transform.localPosition = new Vector3(-0.5f, 1f, 0);
+            
+            /* other code I was working on to get an understanding of the transform.position / parent
+             (it is very confusing for learning)
+             if (usableItem != null)
+            {
+                // logs to the console if the game object isn't null
+                Debug.Log("Usable Item Position: " + usableItem.transform.position);
+
+                // sets the game object to be a child of this 'player' object
+                usableItem.transform.SetParent(this.transform);
+            }
+            */
+        }
     }
 }
